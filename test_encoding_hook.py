74de491a4826f5efda255e791fd2f99387f71fbb
tapiapil <tapiapil@amazon.com>
2026-03-17 01:45:57 +0100

    Test: verify pre-commit hook

---
+++ test_encoding_hook.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Test file to verify pre-commit hook works"""
+
+# This file has Unicode quotes that should be auto-fixed
+message = "Hello World"  # Smart quotes
+arrow = "Move -> Forward"  # Unicode arrow
+status = "[EMOJI] Success"  # Emoji
+
+print(message)
+print(arrow)
+print(status)
